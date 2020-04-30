25c796296330196a2dbe8633d6418885d2a2c775
Romain Kurtz <RomainKurtz@users.noreply.github.com>
2020-04-30 19:26:43 +0200

    Mirror local video

--- public/script.js
+++ public/script.js
@@ -79,6 +79,7 @@ function startWebRTC(isOfferer) {
     audio: true,
     video: true,
   }).then(stream => {
+    
     // Display your local video in #localVideo element
     localVideo.srcObject = stream;
     // Add your stream to be sent to the conneting peer
